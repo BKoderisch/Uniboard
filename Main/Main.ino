4950cce0444d3347983c952da96aae0a97776c22
Maxklos <herzognikolaus@gmail.com>
2018-03-29 11:17:09 +0200

    Speedmeeting Changes 20180113

--- Main/Main.ino
+++ Main/Main.ino
@@ -14,8 +14,9 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
+
 #define mqtt_server "52.58.176.229"
-#define sensor_topic "server/test"
+#define sensor_topic "sensorNode1/temperature"
 
 // create objects
 AnalogTemp tempSensor(0);
@@ -28,7 +29,6 @@ long lastMsg = 0;
 float temp = 0.0;
 float hum = 0.0;
 float diff = 1.0;
-int ledPin = 15;
 
 /**
 * Functions will be called when the connection is lost
@@ -64,21 +64,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  String message = "";
   for (int i=0;i<length;i++) {
-    message = message + (char)payload[i];
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  Serial.println(message);
-  if(message.equals("on")){
-    digitalWrite(ledPin, HIGH);
-    Serial.println("an");
-  } else if (message.equals("off")){
-    digitalWrite(ledPin, LOW);
-    Serial.println("aus");
-  }
+  statusLed.blink(2, 500);
 }
 
 /**
@@ -86,12 +76,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
 * Contains initialisations
 */
 void setup() {
-  Serial.begin(115200);
   WiFiManager wifiManager;
   wifiManager.autoConnect("AutoConnectAP");
+  Serial.begin(115200);
   client.setServer(mqtt_server, 1883);  
   client.setCallback(callback);
-  pinMode(ledPin, OUTPUT);
 }
 
 /**
